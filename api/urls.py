dcc7e54b4c6e43de6622c8018f0bbe18f002c64f
azaz-hassan-ts <86603507+azaz-hassan-ts@users.noreply.github.com>
2021-07-12 18:41:50 +0500

    Done with register, login and logout with token auth

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token 
+
 
 app_name = "api"
 
--- api/views.py
+++ api/views.py
@@ -3,63 +3,49 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics, serializers, status, viewsets, views
-<<<<<<< HEAD
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import authenticate, login, logout
-=======
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate, login
->>>>>>> login
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import authentication_classes, permission_classes
 from .serializers import RegistrationSerializer, LoginSerializer, LogoutSerializer
 from rest_framework.authentication import BasicAuthentication
 import requests
-<<<<<<< HEAD
 from django.contrib import auth
-=======
 from rest_framework.authtoken.models import Token
-
->>>>>>> login
 
 # Create your views here.
 def homepage(request):
     return HttpResponse("Hello World")
 
+class LoginView(ObtainAuthToken):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
 
-# class LoginView(ObtainAuthToken):
-#     queryset = User.objects.all()
-#     permission_classes = (AllowAny,)
-#     serializer_class = LoginSerializer
-#     parser_classes = [JSONParser]
-
-#     def post(self, request):
-#         if request.data == {}:
-#             return Response(
-#                 {"message": "Send request Body"}, status=status.HTTP_204_NO_CONTENT
-#             )
-
-#         username = request.data["username"]
-#         password = request.data["password"]
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             # url = "http://localhost:8000/api-token-auth/"
-#             # data = {"username": username, "password": password}
-#             # token = requests.post(url, data=data)
-#             token = Token.objects.get_or_create(user=username)
-#             login(request, user)
-#             # token = token.text.split('\\')
-#             return Response({
-#                 "token": token.key
-#                 }, status=status.HTTP_200_OK)
-#         else:
-#             return Response(
-#                 {"message": "User/password doesn't match"},
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-
+    def post(self, request, *args, **kwargs):
+        if request.data == {}:
+            return Response(
+                {"message": "Send request Body"}, status=status.HTTP_204_NO_CONTENT
+            )
+        username = request.data["username"]
+        password = request.data["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            login(request, user)
+            return Response({
+                "token": token.key
+                }, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"message": "User/password doesn't match"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -86,8 +72,12 @@ class RegisterView(generics.CreateAPIView):
 
 
 class LogoutView(generics.CreateAPIView):
-    authentication_classes = (IsAuthenticated,)
+    queryset = User.objects.all()
+    premission_classes = (AllowAny, )
+    serializer_class = LogoutSerializer
 
     def get(self, request, format=None):
         request.user.auth_token.delete()
-        return Response(status=status.HTTP_200_OK)
+        return Response({
+            'message': "You are successfully logged out"
+        }, status=status.HTTP_200_OK)
